07100f7d9dac10dc58f5f9459c1565bf62f357e3
William Starr <wcs12@pitt.edu>
2017-10-07 16:29:03 -0400

    hi

--- templates/js/diary.js
+++ templates/js/diary.js
@@ -2,5 +2,5 @@ function save(){
 	console.log("save");
 	document.getElementById("diary").value = '';
 //	confirm("Submitted!");
-	window.location = "file:///C:/Users/wstarr/Desktop/khe/diary.html";
+	window.location = "diary.html";
 }
