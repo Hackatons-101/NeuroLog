dfa3c80beb3fc6fc20d922166ed328a009f27a15
PRATYAKSH GUPTA <pratyaksh1615@gmail.com>
2020-07-21 05:44:26 +0530

    uploading files

---
+++ js/diary.js
@@ -0,0 +1,6 @@
+function save(){
+	var usernameDiary;
+
+	document.getElementById("diary").value = '';
+	window.location = "diary.html";
+}
--- templates/js/diary.js
+++
@@ -1,6 +0,0 @@
-function save(){
-	var usernameDiary;
-
-	document.getElementById("diary").value = '';
-	window.location = "diary.html";
-}
